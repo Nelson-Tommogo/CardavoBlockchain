89d0b9a6f66b26ee3a0f050579f5713a10bc09c7
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-11 16:49:11 +0300

    Cardavo Haskell

--- readfile.hs
+++ readfile.hs
@@ -2,8 +2,7 @@ import System.IO
 
 main :: IO ()
 main = do
-    let filePath = "//home/common/Downloads/style.css"  -- Replace this with the actual path
+    let filePath = "//home/common/Downloads/style.css"  
     withFile filePath ReadMode $ \handle -> do
         contents <- hGetContents handle
         putStrLn contents
-    -- File is automatically closed after this point
