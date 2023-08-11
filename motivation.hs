5ea03e98a0caeeffc4c19d94f137e166108de260
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-11 23:57:37 +0300

    Cardavo Haskell

--- motivation.hs
+++ motivation.hs
@@ -2,6 +2,6 @@ main :: IO ()
 main = do
     let failedBefore = True
     let message = if failedBefore
-                    then "If you have to fail again, fail better than you did before."
-                    else "You haven't failed before, so give it your best shot!"
+        then "If you have to fail again, fail better than you did before."
+        else "You haven't failed before, so give it your best shot!"
     putStrLn message
