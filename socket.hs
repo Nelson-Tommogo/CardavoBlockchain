26bd05226c7df8d3415fa891ee8df511ab22acb5
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-16 15:52:11 +0300

    Block Chain

---
+++ socket.hs
@@ -0,0 +1,42 @@
+import Network.Socket
+
+main :: IO ()
+main = withSocketsDo $ do
+    addr <- resolve "5000"  -- Resolve the address for port 3000
+    sock <- openSocket addr   -- Open a socket using the resolved address
+    bind sock (addrAddress addr)  -- Bind the socket to the address
+    listen sock 10  -- Listen for incoming connections with a maximum queue of 10
+
+    putStrLn "Server is listening on port 5000..."
+
+    -- Accept and handle incoming connections
+    acceptConnections sock
+
+-- Resolve the address
+resolve :: String -> IO AddrInfo
+resolve port = do
+    let hints = defaultHints { addrFlags = [AI_PASSIVE], addrSocketType = Stream }
+    head <$> getAddrInfo (Just hints) Nothing (Just port)
+
+-- Open a socket using the resolved address
+openSocket :: AddrInfo -> IO Socket
+openSocket addr = do
+    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+    setSocketOption sock ReuseAddr 1
+    return sock
+
+-- Accept and handle incoming connections
+acceptConnections :: Socket -> IO ()
+acceptConnections sock = do
+    (clientSock, clientAddr) <- accept sock
+    putStrLn $ "Connection from " ++ show clientAddr
+    handleConnection clientSock
+    acceptConnections sock
+
+-- Handle an incoming connection
+handleConnection :: Socket -> IO ()
+handleConnection clientSock = do
+    msg <- recv clientSock 1024
+    putStrLn $ "Received: " ++ show msg
+    sendAll clientSock "Hello from the server!"
+    close clientSock
--- tree.hs
+++ tree.hs
@@ -1,8 +1,21 @@
-data Tree leaf a | Node (Tree a) (Tree a)
-deriving Show
+-- Define a Tree data type
+data Tree a = Node a [Tree a]
+  deriving Show
 
-label :: Tree -> Tree (Int, a)
-label = snd . label' 1
+-- Create a sample tree:
+--      1
+--     /|\
+--    2 3 4
+--   /
+--  5
+sampleTree :: Tree Int
+sampleTree =
+    Node 1
+        [ Node 2 [Node 5 []]
+        , Node 3 []
+        , Node 4 []
+        ]
 
-label'  :: Int -> Tree a -> (Int, Tree(Int a))
-label' nextLabel (leaf a) = (nextLabel +1, leaf(nextLabel))
+-- Main function
+main :: IO ()
+main = print sampleTree
