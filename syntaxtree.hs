54cd837076ce630d71c1d2d73e04889905863f91
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-18 16:52:53 +0300

    Block Chain

--- syntaxtree.hs
+++ syntaxtree.hs
@@ -1,6 +1,5 @@
 import Data.Tree
 
--- Data type to represent arithmetic expressions
 data Expr = Lit Int
           | Add Expr Expr
           | Sub Expr Expr
@@ -8,7 +7,6 @@ data Expr = Lit Int
           | Div Expr Expr
           deriving Show
 
--- Function to convert an Expr into a syntax tree
 toSyntaxTree :: Expr -> Tree String
 toSyntaxTree (Lit n) = Node (show n) []
 toSyntaxTree (Add e1 e2) = Node "+" [toSyntaxTree e1, toSyntaxTree e2]
@@ -16,7 +14,6 @@ toSyntaxTree (Sub e1 e2) = Node "-" [toSyntaxTree e1, toSyntaxTree e2]
 toSyntaxTree (Mul e1 e2) = Node "*" [toSyntaxTree e1, toSyntaxTree e2]
 toSyntaxTree (Div e1 e2) = Node "/" [toSyntaxTree e1, toSyntaxTree e2]
 
--- Example expression: (3 + 2) * 4
 expr :: Expr
 expr = Mul (Add (Lit 3) (Lit 2)) (Lit 4)
 
