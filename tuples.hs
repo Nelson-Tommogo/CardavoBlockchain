640992d2bd06aa2c7efee4bb5f26c41a8a2f80c7
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-31 12:09:40 +0300

    Haskel

--- tuples.hs
+++ tuples.hs
@@ -1,5 +1,17 @@
-tuple1 :: (double, char)
+tuple1 :: (Double, Char)
 tuple1 = (4.667, 'b')
 
-tuple2 :: (String, int, String, Bool)
-tuple1 = ("Nelson", 22, "Years", True)
+firstElement :: Double
+firstElement = fst tuple1
+
+tuple2 :: (String, Int, String, Bool)
+tuple2 = ("Nelson", 22, "Years", True)
+
+firstElement2 :: String
+firstElement2 = case tuple2 of
+    (x, _, _, _) -> x
+
+main :: IO ()
+main = do
+    putStrLn $ "First element of tuple1: " ++ show firstElement
+    putStrLn $ "First element of tuple2: " ++ firstElement2
