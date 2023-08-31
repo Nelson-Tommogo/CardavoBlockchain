428e39339dba35986fb3efa959b2763c7afb2784
Tommogo-coder <nelsontommogo9@gmail.com>
2023-08-31 12:56:47 +0300

    Haskell Lists

--- lists.hs
+++ lists.hs
@@ -1,2 +1,14 @@
-units :: [int]
-units = [68,77,78, 99, 59, 93, 91,90]
+units :: [Int]
+units = [68, 77, 78, 99, 59, 93, 91, 90]
+
+totalMarksUnits :: [Int]
+totalMarksUnits = [x + x + x + x + x + x + x + x | x <- units]
+
+main :: IO ()
+main = do
+    putStrLn "Units:"
+    print units
+    
+    putStrLn "Total Marks for Units:"
+    print totalMarksUnits
+
