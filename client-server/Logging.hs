77bd978e03bcffe464f2b65fd57e4e777397e0f2
Tommogo-coder <nelsontommogo9@gmail.com>
2023-09-14 17:17:19 +0300

    Haskell

---
+++ client-server/Logging.hs
@@ -0,0 +1,9 @@
+module Logging where
+
+import System.Time (getCurrentTime)
+
+-- Function to log a transaction with a timestamp
+logTransaction :: Tx -> IO ()
+-- Implementation of logTransaction...
+
+-- Implement other logging functions as needed
