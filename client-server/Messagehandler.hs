b414a7bfd7aaccd8c7b705c21c19279610c2e869
Tommogo-coder <nelsontommogo9@gmail.com>
2023-09-14 17:06:35 +0300

    Haskell

---
+++ client-server/Messagehandler.hs
@@ -0,0 +1,7 @@
+module MessageHandling where
+
+import NetworkManagement (Peer)
+
+-- Function to handle incoming messages
+handleMessage :: Message -> Peers -> IO ()
+-- Implementation of handleMessage...
